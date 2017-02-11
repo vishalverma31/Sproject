2bec304fe29e6909a3d6b25c03455cdce52c72a9
Vishal Verma <vishalverma31093@gmail.com>
2017-02-12 01:17:38 +0530

    Sproject
    Complete Project

--- sprojectRest/src/main/java/com/spa/sprojectRest/config/WebAppInitializer.java
+++ sprojectRest/src/main/java/com/spa/sprojectRest/config/WebAppInitializer.java
@@ -1,25 +1,29 @@
 package com.spa.sprojectRest.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
+public class WebAppInitializer implements WebApplicationInitializer {
+	
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext context=new AnnotationConfigWebApplicationContext();
+		context.register(WebConfig.class,WebSocketConfig.class,ImageUploadApp.class);
 		
-		return new Class[]{WebConfig.class,WebSocketConfig.class,ImageUploadApp.class};
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected String[] getServletMappings() {
+		context.setServletContext(container);
 		
-		return new String[]{"/","/rest/*"};
+		ServletRegistration.Dynamic servlet=container.addServlet("dispatcher", new DispatcherServlet(context));
+		
+		servlet.addMapping("/");
+		servlet.addMapping("/rest/*");
+		
+		
+		
+		
 	}
 
 }
